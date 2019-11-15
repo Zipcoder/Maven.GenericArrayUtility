36ce8dfec40a9de5bc22bcf776565edad1c657b4
wes <wes.jones1597@gmail.com>
2019-11-15 11:56:38 -0500

    getNumberOfOccurrences tests passing but nothing else

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,40 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E> {
+    private E[] array;
+
+    public ArrayUtility (E[] array) {
+        this.array = array;
+    }
+    // TODO figure out how to prevent casting to Object
+    public E[] removeValue(E valueToRemove) {
+//        E[] arr = (E[]) new Object[array.length-getNumberOfOccurrences(valueToRemove)];
+//        ArrayList<E> listo = new ArrayList<>();
+//        for (E item: array) {
+//            if (!(valueToRemove).equals(item)) listo.add(item);
+//        }
+        return null;
+    }
+
+    public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToCheck) {
+        return null;
+    }
+
+    public E getMostCommonFromMerge(E[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(E valueToCheck) {
+        return ((int) Arrays.stream(array)
+                .filter(val -> val == valueToCheck)
+                .count());
+    }
 }
