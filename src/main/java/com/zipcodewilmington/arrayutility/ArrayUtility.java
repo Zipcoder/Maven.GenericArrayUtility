588a9fa7daae6badc7a51a29d23fea9088752b11
wes <wes.jones1597@gmail.com>
2019-11-15 12:57:18 -0500

    countDuplicatesInMerge, removeValue tests passing

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -14,18 +14,28 @@ public class ArrayUtility<E> {
     public ArrayUtility (E[] array) {
         this.array = array;
     }
+
     // TODO figure out how to prevent casting to Object
     public E[] removeValue(E valueToRemove) {
-//        E[] arr = (E[]) new Object[array.length-getNumberOfOccurrences(valueToRemove)];
-//        ArrayList<E> listo = new ArrayList<>();
-//        for (E item: array) {
-//            if (!(valueToRemove).equals(item)) listo.add(item);
-//        }
-        return null;
+        E[] arr = Arrays.copyOf(array, array.length-getNumberOfOccurrences(valueToRemove));
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!array[i].equals(valueToRemove)) {
+                arr[index] = array[i];
+                index++;
+            }
+        }
+        return arr;
     }
 
     public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToCheck) {
-        return null;
+        E[] bigArr = Arrays.copyOf(array, array.length + arrayToMerge.length);
+        for (int i = array.length; i < bigArr.length; i++) {
+            bigArr[i] = arrayToMerge[i-array.length];
+        }
+        return (int) Arrays.stream(bigArr)
+                .filter(val -> val == valueToCheck)
+                .count();
     }
 
     public E getMostCommonFromMerge(E[] arrayToMerge) {
