c8dee443b890887b19dafb18bc2ee29aa8c58fc8
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-15 17:26:43 -0500

    Created method stubs

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,34 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+
+    /*
+    Added <T> to the class signature
+    Created constructor with the generic input
+    Changed the countDup method to accept T type
+
+    Return generic for getMostCommon/remove as it it used with different types
+     */
+
+
+    public ArrayUtility(T[] inputArray) {
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+
+    }
 }
