3e70b2f41e5ea8aca3c44d5a9f03873eff0909c7
MannyMonocle <eetumadu@gmail.com>
2019-11-28 18:37:55 -0500

    first write

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,17 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<D extends  Object> {
+    protected D[] inputArray;
+
+    public ArrayUtility(D[] inputArray){
+        this.inputArray = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(D[] arrayToMerge, D valueToEvaluate){
+        Integer count = 0;
+        for(D element : inputArray){if(element == valueToEvaluate){ count++;}}
+        for(D element : arrayToMerge){if(element == valueToEvaluate){ count++;}}
+        return count;
+    }
 }
