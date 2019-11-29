e21b5f7876efc40f8c2fb93730b02f7751178264
MannyMonocle <eetumadu@gmail.com>
2019-11-29 00:12:23 -0500

    Generics

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -10,31 +13,57 @@ public class ArrayUtility<D extends  Object> {
         this.inputArray = inputArray;
     }
 
-    public Integer countDuplicatesInMerge(D[] arrayToMerge, D valueToEvaluate){
+    public Integer count(D[] array, D valueToCount){
         Integer count = 0;
-        for(D element : inputArray){if(element == valueToEvaluate){ count++;}}
-        for(D element : arrayToMerge){if(element == valueToEvaluate){ count++;}}
+        for(D element : array){if(element == valueToCount){ count++;}}
         return count;
     }
 
-    public D getMostCommonFromMerge(D[] arrayToMerge) {
-        D currentAns = null;
-        Integer highestCount = 0;
-        Integer otherCount = 0;
 
-        for (D element : inputArray) {
-            if (countDuplicatesInMerge(arrayToMerge, element) > highestCount) {
-                currentAns = element;
-                highestCount = countDuplicatesInMerge(arrayToMerge, element);
-            }
-        }
-
-        for (D element : arrayToMerge) {
-            if (countDuplicatesInMerge(arrayToMerge, element) > highestCount) {
-                currentAns = element;
-                highestCount = countDuplicatesInMerge(arrayToMerge, element);
-            }
-        }
-        return currentAns;
+    public Integer getNumberOfOccurrences(D valueToEvaluate){
+        return count(inputArray, valueToEvaluate);
     }
+
+    public D[] removeValue(D valueToRemove){
+        return removeValue(inputArray, valueToRemove);
+    }
+
+    public D[] removeValue(D[] array, D valueToRemove){
+        Integer newSize = array.length - count(array,valueToRemove);
+        D[] newArray = (D[]) Array.newInstance(valueToRemove.getClass(),newSize);
+        Integer w = 0;
+        for (int i = 0; i < array.length; i++){
+            if(array[i] != valueToRemove){
+                newArray[w] = array[i];
+                w++;
+            }
+        }
+        return newArray;
+    }
+
+    public Integer countDuplicatesInMerge(D[] arrayToMerge, D valueToEvaluate){
+        return count(arrayToMerge, valueToEvaluate) + getNumberOfOccurrences(valueToEvaluate);
+    }
+
+    public D getMostCommonFromMerge(D[] arrayToMerge) {
+        D inputMostCommon = getMostCommon(inputArray);
+        Integer inputCommonCount = count(inputArray,inputMostCommon);
+        D arrayMostCommon = getMostCommon(arrayToMerge);
+        Integer arrayCommonCount = count(arrayToMerge, arrayMostCommon);
+        return (inputCommonCount > arrayCommonCount) ? inputMostCommon : arrayMostCommon;
+    }
+
+    public D getMostCommon(D[] array){
+        Integer highestCount = 0;
+        D mostCommon = null;
+        for(D element : array){
+            if(count(array,element) > highestCount){
+                mostCommon = element;
+                highestCount = count(array,element);
+            }
+        }
+        return mostCommon;
+    }
+
+
 }
