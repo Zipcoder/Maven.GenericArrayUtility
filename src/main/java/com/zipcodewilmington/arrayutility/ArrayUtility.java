dbb28afa20308d0845cadfe5fc0fb5c34bb0288e
jorge <jorgetapia0293@gmail.com>
2021-04-04 12:47:14 -0400

    all but removeValue pass

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,63 @@
 package com.zipcodewilmington.arrayutility;
 
+import org.omg.CORBA.Object;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    T[] inputArray;
+
+    public ArrayUtility(T[] array){
+        this.inputArray = array;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        Integer counter = getNumberOfOccurrences(valueToEvaluate);
+        for(T t: arrayToMerge){
+            if(t == valueToEvaluate){ counter++; }
+        }   return counter;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        T mostOccuring = null, temp;
+        int counter = 0, holder = 0;
+        for(int i = 0; i < arrayToMerge.length; i++ ){
+            temp = arrayToMerge[i];
+            for(int j = 0; j < arrayToMerge.length; j++){
+                if(temp.equals(arrayToMerge[j])){
+                    counter++;
+                }
+            }
+            if(counter > holder){
+                holder = counter;
+                mostOccuring = arrayToMerge[i];
+            }
+            counter = 0;
+        }
+        return mostOccuring;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        Integer counter = 0;
+        for(T t: inputArray){
+            if(t == valueToEvaluate){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        Integer occurs = getNumberOfOccurrences(valueToRemove);
+        T[] newT = (T[]) new Object[inputArray.length - occurs];
+        int j = 0;
+        for(T t: inputArray){
+            if(t != valueToRemove){
+                newT[j] = t;
+                j++;
+            }
+        } return newT;
+    }
 }
